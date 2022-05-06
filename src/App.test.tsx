b023104e11eafe8f78f67b3fc4faae27e34f9583
rishabhj247 <rishabh.j@chapter247.com>
2022-05-06 10:20:01 +0530

    added basic test cases with Jest

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('increment count by one when +1 is clicked', () => {
+  const { getByText, getByTestId } = render(<App />);
+
+  fireEvent.click(getByText('+1'));
+  let num = getByText('1');
+  expect(num).toBeInTheDocument();
+});
+it('decrement count by one when down is clicked', () => {
+  const { getByText, getByTestId } = render(<App />);
+  //  fireEvent.click(getByText('+1'));
+  fireEvent.click(getByText('-1'));
+  let num = getByText('0');
+  expect(num).toBeInTheDocument();
 });
